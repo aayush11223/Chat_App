446a95882c27fed3b37e7ded3469256e12f6c516
Aayush Basnet <aayush9823@gmail.com>
2026-05-14 23:01:33 +0545

    safe code again, chaged name of utilites and router files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/index.js'
+import router from './router/router.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
